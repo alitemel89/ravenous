7c590803ffc880cfc1830b62dc950c72b6ea80fc
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-10-02 15:48:44 +0300

    add handleTermChange and handleTermChange methods

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -33,6 +33,18 @@ class SearchBar extends React.Component {
         })
     }
 
+    handleTermChange(event) {
+        this.setState({
+            term: event.target.value
+        })
+    }
+
+    handleLocationChange(event) {
+        this.setState({
+            location: event.target.value
+        })
+    }
+
     renderSortByOptions () {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption]
