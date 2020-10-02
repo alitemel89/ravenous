882db778ec7fe0c4fe70999ed511577ad2749895
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-10-02 16:33:01 +0300

    add render method to SearchBar component in App.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,6 +17,10 @@ class SearchBar extends React.Component {
             'Highest Rated': 'rating',
             'Most Reviewed': 'review_count'
         }
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
     getSortByClass(sortByOption) {
@@ -45,6 +49,11 @@ class SearchBar extends React.Component {
         })
     }
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        event.preventDefault();
+    }
+
     renderSortByOptions () {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption]
@@ -61,11 +70,11 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a href="www.#.com">Let's Go</a>
+                    <a href="www.#.com" onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
 
